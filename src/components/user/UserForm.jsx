d87f8e514a94e2486ed852f6063ac95bec59bd1c
d_flex <hiyoo95@gmail.com>
2022-01-15 23:18:11 +0900

    app deployed to heroku cloud server

--- src/components/user/UserForm.jsx
+++ src/components/user/UserForm.jsx
@@ -1,13 +1,13 @@
-import { useHistory } from "react-router-dom";
-import { useContext} from "react";
-import UserContext from "../store/user-ctx";
-import ModalContext from "../store/modal-ctx";
-import Input from "../layout/ui/input/Input";
-import Highlight from "../layout/ui/text/Hightligtht";
+import { useHistory } from 'react-router-dom';
+import { useContext } from 'react';
+import UserContext from '../store/user-ctx';
+import ModalContext from '../store/modal-ctx';
+import Input from '../layout/ui/input/Input';
+import Highlight from '../layout/ui/text/Hightligtht';
 
-import classes from "./UserForm.module.css";
-import btnClasses from "../layout/ui/button/Button.module.css";
-import iptClasses from "../layout/ui/input/Input.module.css";
+import classes from './UserForm.module.css';
+import btnClasses from '../layout/ui/button/Button.module.css';
+import iptClasses from '../layout/ui/input/Input.module.css';
 import txtClasses from '../layout/ui/text/Highlight.module.css';
 
 const UserForm = () => {
@@ -29,58 +29,57 @@ const UserForm = () => {
   const handleFormSubmit = (e) => {
     e.preventDefault();
     if (invalidName && !ctx.userGender) {
-      modalCtx.onSetMessage("잘못된 이름입니다! & 성별을 선택해주세요!");
+      modalCtx.onSetMessage('잘못된 이름입니다! & 성별을 선택해주세요!');
       modalCtx.onSetModal(true);
     } else if (!ctx.userName) {
       modalCtx.onSetMessage('이름을 입력해주세요!');
       modalCtx.onSetModal(true);
     } else if (!ctx.userGender) {
-      modalCtx.onSetMessage("성별을 선택해주세요!");
+      modalCtx.onSetMessage('성별을 선택해주세요!');
       modalCtx.onSetModal(true);
     } else if (invalidName) {
-      modalCtx.onSetMessage("올바른 이름이 아닙니다!");
+      modalCtx.onSetMessage('올바른 이름이 아닙니다!');
       modalCtx.onSetModal(true);
     } else {
-      history.push("/example");
+      history.push('/example');
     }
   };
-  
 
   return (
-      <form onSubmit={handleFormSubmit}>
-        <div className={classes.name}>
+    <form onSubmit={handleFormSubmit}>
+      <div className={classes.name}>
         <Highlight className={txtClasses.label}>이름</Highlight>
         <Input
-          type="text"
+          type='text'
           value={ctx.userName}
-          name="name-input"
+          name='name-input'
           onChange={handleNameChange}
         />
-        </div>
+      </div>
+      <div className={classes.genderSelect}>
+        <Highlight className={txtClasses.label}>성별</Highlight>
         <div className={classes.genderSelect}>
-          <Highlight className={txtClasses.label}>성별</Highlight>
-          <div className={classes.genderSelect}>
-            <Input
-              className={`${iptClasses.control}${
-                !invalidName === false ? iptClasses.invalid : ""
-              }`}
-              type="radio"
-              name="gender-select"
-              value="남자"
-              onClick={handleGenderSelect}
-            />
-            <Input
-              type="radio"
-              name="gender-select"
-              value="여자"
-              onClick={handleGenderSelect}
-            />
-          </div>
+          <Input
+            className={`${iptClasses.control}${
+              !invalidName === false ? iptClasses.invalid : ''
+            }`}
+            type='radio'
+            name='gender-select'
+            value='남자'
+            onClick={handleGenderSelect}
+          />
+          <Input
+            type='radio'
+            name='gender-select'
+            value='여자'
+            onClick={handleGenderSelect}
+          />
         </div>
-        <button className={btnClasses.btn} disabled={ctx.isFormValid}>
-        <span style={{fontWeight:'bold'}}>검사하기</span>
-        </button>
-      </form>
+      </div>
+      <button className={btnClasses.btn} disabled={ctx.isFormValid}>
+        <span style={{ fontWeight: 'bold' }}>검사하기</span>
+      </button>
+    </form>
   );
 };
 
