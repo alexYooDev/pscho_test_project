260382a619f1a939592d5422d16021852660aee0
d_flex <hiyoo95@gmail.com>
2022-01-15 23:15:22 +0900

    app deployed to heroku cloud server

---
+++ src/components/data-src/data-src.jsx
@@ -0,0 +1,8 @@
+export const API_KEY = 'a72cbf8ac59de7c7968bd99e69950998';
+export const CAT_NUM = "6";
+export const MAN = "100323";
+export const WOMAN = "100324";
+export const TRGET_SEQ = "100209";
+
+export const TEST_URL = `https://www.career.go.kr/inspct/openapi/test/questions?apikey=${API_KEY}&q=${CAT_NUM}`;
+export const RESULT_URL = `https://www.career.go.kr/inspct/openapi/test/report?apikey=${API_KEY}&qestrnSeq=${CAT_NUM}`;
